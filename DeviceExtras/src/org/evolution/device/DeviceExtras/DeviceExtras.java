topes1/device_oneplus_sdm845-common

sdm845-common: DeviceExtras: Add earpiece/microphone gain preferences
Signed-off-by: AnierinB <anierin@evolution-x.org>

DeviceExtras/src/org/spark/device/DeviceExtras/DeviceExtras.java
@@ -68,6 +68,7 @@ public class DeviceExtras extends PreferenceFragment
     public static final String KEY_DC_SWITCH = "dc";
     public static final String KEY_DCI_SWITCH = "dci";
     public static final String KEY_DOZE = "advanced_doze_settings";
+    public static final String KEY_EAR_GAIN = "earpiece_gain";
     public static final String KEY_FPS_INFO = "fps_info";
     public static final String KEY_FPS_INFO_POSITION = "fps_info_position";
     public static final String KEY_FPS_INFO_COLOR = "fps_info_color";
@@ -75,6 +76,7 @@ public class DeviceExtras extends PreferenceFragment
     public static final String KEY_GAME_SWITCH = "game_mode";
     public static final String KEY_HBM_SWITCH = "hbm";
     public static final String KEY_KCAL = "kcal";
+    public static final String KEY_MIC_GAIN = "microphone_gain";
     public static final String KEY_SRGB_SWITCH = "srgb";
     public static final String KEY_USB2_SWITCH = "usb2_fast_charge";
     public static final String KEY_WIDE_SWITCH = "wide";
@@ -92,11 +94,13 @@ public class DeviceExtras extends PreferenceFragment
     private static TwoStatePreference mUSB2FastChargeModeSwitch;
 
     private CustomSeekBarPreference mFpsInfoTextSizePreference;
+    private EarGainModeSwitch mEarGain;
     private Preference mDozeSettings;
     private Preference mKcal;
     private ListPreference mBottomKeyPref;
     private ListPreference mMiddleKeyPref;
     private ListPreference mTopKeyPref;
+    private MicGainModeSwitch mMicGain;
     private VibratorStrengthPreference mVibratorStrength;
     private VibratorCallStrengthPreference mVibratorCallStrength;
     private VibratorNotifStrengthPreference mVibratorNotifStrength;
@@ -156,6 +160,18 @@ public class DeviceExtras extends PreferenceFragment
 
         // Slider Preferences
         initNotificationSliderPreference();
+
+        // Earpiece gain
+        mEarGain = (EarGainModeSwitch) findPreference(KEY_EAR_GAIN );
+        if (mEarGain != null) {
+            mEarGain.setEnabled(EarGainModeSwitch.isSupported());
+        }
+
+        // Microphone gain
+        mMicGain = (MicGainModeSwitch) findPreference(KEY_MIC_GAIN );
+        if (mMicGain != null) {
+            mMicGain.setEnabled(MicGainModeSwitch.isSupported());
+        }
 
         // USB2 Force FastCharge
         mUSB2FastChargeModeSwitch = (TwoStatePreference) findPreference(KEY_USB2_SWITCH);

DeviceExtras/src/org/spark/device/DeviceExtras/Startup.java
@@ -35,8 +35,10 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent bootintent) {
 
         DeviceExtras.restoreSliderStates(context);
+        EarGainModeSwitch.restore(context);
         org.spark.device.DeviceExtras.doze.DozeUtils.checkDozeService(context);
         org.spark.device.DeviceExtras.kcal.KCalSettings.restore(context);
+        MicGainModeSwitch.restore(context);
         VibratorCallStrengthPreference.restore(context);
         VibratorNotifStrengthPreference.restore(context);
         VibratorStrengthPreference.restore(context);

He-Is-HaZaRdOuS/AB_device_oneplus_sdm845-common

DeviceExtras/src/org/evolution/device/DeviceExtras/DeviceExtras.java
@@ -68,6 +68,7 @@ public class DeviceExtras extends PreferenceFragment
     public static final String KEY_DC_SWITCH = "dc";
     public static final String KEY_DCI_SWITCH = "dci";
     public static final String KEY_DOZE = "advanced_doze_settings";
+    public static final String KEY_EAR_GAIN = "earpiece_gain";
     public static final String KEY_FPS_INFO = "fps_info";
     public static final String KEY_FPS_INFO_POSITION = "fps_info_position";
     public static final String KEY_FPS_INFO_COLOR = "fps_info_color";
@@ -75,6 +76,7 @@ public class DeviceExtras extends PreferenceFragment
     public static final String KEY_GAME_SWITCH = "game_mode";
     public static final String KEY_HBM_SWITCH = "hbm";
     public static final String KEY_KCAL = "kcal";
+    public static final String KEY_MIC_GAIN = "microphone_gain";
     public static final String KEY_SRGB_SWITCH = "srgb";
     public static final String KEY_USB2_SWITCH = "usb2_fast_charge";
     public static final String KEY_WIDE_SWITCH = "wide";
@@ -92,11 +94,13 @@ public class DeviceExtras extends PreferenceFragment
     private static TwoStatePreference mUSB2FastChargeModeSwitch;
 
     private CustomSeekBarPreference mFpsInfoTextSizePreference;
+    private EarGainPreference mEarGain;
     private Preference mDozeSettings;
     private Preference mKcal;
     private ListPreference mBottomKeyPref;
     private ListPreference mMiddleKeyPref;
     private ListPreference mTopKeyPref;
+    private MicGainPreference mMicGain;
     private VibratorStrengthPreference mVibratorStrength;
     private VibratorCallStrengthPreference mVibratorCallStrength;
     private VibratorNotifStrengthPreference mVibratorNotifStrength;
@@ -156,6 +160,18 @@ public class DeviceExtras extends PreferenceFragment
 
         // Slider Preferences
         initNotificationSliderPreference();
+
+        // Earpiece gain
+        mEarGain = (EarGainPreference) findPreference(KEY_EAR_GAIN);
+        if (mEarGain != null) {
+            mEarGain.setEnabled(EarGainPreference.isSupported());
+        }
+
+        // Microphone gain
+        mMicGain = (MicGainPreference) findPreference(KEY_MIC_GAIN);
+        if (mMicGain != null) {
+            mMicGain.setEnabled(MicGainPreference.isSupported());
+        }
 
         // USB2 Force FastCharge
         mUSB2FastChargeModeSwitch = (TwoStatePreference) findPreference(KEY_USB2_SWITCH);

DeviceExtras/src/org/evolution/device/DeviceExtras/Startup.java
@@ -35,8 +35,10 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent bootintent) {
 
         DeviceExtras.restoreSliderStates(context);
+        EarGainPreference.restore(context);
         org.evolution.device.DeviceExtras.doze.DozeUtils.checkDozeService(context);
         org.evolution.device.DeviceExtras.kcal.KCalSettings.restore(context);
+        MicGainPreference.restore(context);
         VibratorCallStrengthPreference.restore(context);
         VibratorNotifStrengthPreference.restore(context);
         VibratorStrengthPreference.restore(context);